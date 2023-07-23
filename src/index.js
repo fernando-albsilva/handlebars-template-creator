938fd132d2e4be70193152ae0877ed0fe235f49c
Fernando Albuquerque <fernando.albsilva@gmail.com>
2023-07-23 15:46:33 -0300

    adicionando nodedemon para autorefres on save

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const Handlebars = require("handlebars");
 const templateString = readTemplateFromFile("src/template.hbs");
 const data = readSourceFromFile("src/data.json");
 const compiledTemplate = compileTemplate(templateString, data);
-console.log(compiledTemplate);
+
+consolePrintTemplate(compiledTemplate);
+consolePrintDate();
 
 writeCompiledTemplateToFile(compiledTemplate);
 
@@ -23,4 +25,29 @@ function compileTemplate(templateString, data) {
 
 function writeCompiledTemplateToFile(compiledTemplate) {
     fileSystem.writeFileSync("src/compiled/index.html", compiledTemplate);
+}
+
+function consolePrintTemplate(compiledTemplate) {
+    console.log("***********************");
+    console.log("Compiled template: ");
+    console.log(compiledTemplate);
+    console.log("***********************");
+}
+
+function consolePrintDate() {
+    console.log("///////////////////////////////");
+    console.log("Build in : ");
+    // format date in hh/mm/ss dd/mm/yyyy with new date
+    const date = new Date();
+
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based, so we add 1
+    const year = date.getFullYear();
+
+    console.log(`${hours}:${minutes}:${seconds} ${day}/${month}/${year}`);
+
+    console.log("///////////////////////////////");
 }
